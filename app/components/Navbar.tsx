a2a943872653fda6fef98ef351c0ab58d115a595
oleksandrsavych <respect2203@gmail.com>
2024-08-25 17:49:58 +0300

    Implement Navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+import Link from "next/link";
+import {AiFillBug} from "react-icons/ai";
+import {usePathname} from "next/navigation";
+import classnames from "classnames";
+
+const Navbar = () => {
+    const currentPath = usePathname();
+
+    const links: { label: string, href: string }[] = [
+        { label: "Dashboard", href: "/" },
+        { label: "Issues", href: "/issues" }
+    ];
+
+    return (
+        <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
+            <Link href="/"><AiFillBug /></Link>
+            <ul className="flex space-x-6">
+                {links.map(link => {
+                    return <Link
+                        key={link.href}
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-900 transition-colors': true
+                        })}
+                        href={link.href}>
+                        {link.label}
+                    </Link>
+                })}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
